f607a965a76bc353a6db93ad60bfffa6d3615dd0
Ilya Anatskiy <devvvyoff@gmail.com>
2020-10-18 13:48:32 +0300

    fix

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -5,23 +5,39 @@ interface TodoFormProps {
 }
 
 const TodoForm: React.FC<TodoFormProps> = ({ onAdd }) => {
+  const [error, setError] = React.useState(false);
   const ref = React.useRef<HTMLInputElement>(null);
 
   const clickHandler = () => {
-    onAdd(ref.current!.value);
-    ref.current!.value = '';
+    if (ref.current!.value.trim()) {
+      setError(false);
+      onAdd(ref.current!.value);
+      ref.current!.value = '';
+    } else {
+      setError(!error);
+      ref.current!.value = '';
+    }
   };
 
   return (
-    <div className="input-field mt2 flex">
-      <input ref={ref} className="w80" type="text" id="title" placeholder="Введите название дела" />
-      <label htmlFor="title" className="active">
-        Введите название дела
-      </label>
-      <a className="wwaves-effect waves-light btn brown darken-2 w20" onClick={clickHandler}>
-        Добавить
-      </a>
-    </div>
+    <>
+      <div className="input-field mt2 flex">
+        <input
+          ref={ref}
+          className="w80"
+          type="text"
+          id="title"
+          placeholder="Введите название дела"
+        />
+        <label htmlFor="title" className="active">
+          Введите название дела
+        </label>
+        <a className="wwaves-effect waves-light btn brown darken-2 w20" onClick={clickHandler}>
+          Добавить
+        </a>
+      </div>
+      {error && <span className="error">Заполните поле</span>}
+    </>
   );
 };
 
